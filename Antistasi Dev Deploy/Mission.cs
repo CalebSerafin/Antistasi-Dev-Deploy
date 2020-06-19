38728ab10fb4eb583d1bc2d52c53dd905648fa96
Caleb Serafin <31820984+CalebSerafin@users.noreply.github.com>
2020-06-19 16:15:52 +0200

    Default version number used if stringtable.xml is corrupt. Error expressed to user regardless of visibility settings.

--- Antistasi Dev Deploy/Mission.cs
+++ Antistasi Dev Deploy/Mission.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Xml;
+using static Antistasi_Dev_Deploy.WindowPowerLib;
 
 namespace Antistasi_Dev_Deploy {
 	class Mission {
@@ -11,20 +13,26 @@ namespace Antistasi_Dev_Deploy {
 			}
 			string Dir_StringTable = Dir_AntistasiRoot + "Stringtable.xml";
 
-			string MissionVersion = "";
+			string MissionVersion = "-2-X";
 
 			if (File.Exists(Dir_AntistasiRoot + "Stringtable.xml")) {
 				string StringTableRaw = File.ReadAllText(Dir_StringTable);
 				XmlDocument StringTable = new XmlDocument();
-				StringTable.LoadXml(StringTableRaw);
-				XmlNamespaceManager XMLNamespace = new XmlNamespaceManager(StringTable.NameTable);
-				XmlNodeList StringTable_Node = StringTable.SelectNodes("/Project/Package/Container/Key", XMLNamespace);
-				foreach (XmlNode xNode in StringTable_Node) {
-					if (xNode.Attributes[0].Value == "STR_antistasi_credits_generic_version_text") {
-						MissionVersion = "-" + xNode.InnerText.Replace('.', '-');
+				try {
+					StringTable.LoadXml(StringTableRaw);
+					XmlNamespaceManager XMLNamespace = new XmlNamespaceManager(StringTable.NameTable);
+					XmlNodeList StringTable_Node = StringTable.SelectNodes("/Project/Package/Container/Key", XMLNamespace);
+					foreach (XmlNode xNode in StringTable_Node) {
+						if (xNode.Attributes[0].Value == "STR_antistasi_credits_generic_version_text") {
+							MissionVersion = "-" + xNode.InnerText.Replace('.', '-');
+						}
 					}
-				}
-
+				} catch (System.Xml.XmlException e) {
+					WindowPower.ShowWindow(WindowPower.GetConsoleWindow(), WindowPower.SW_SHOW);
+					Console.WriteLine("ERROR: Malformed stringtable.xml! Please check the last commit made to stringtable.xml.");
+					Console.WriteLine("Packing missions with default version 2-X.");
+					Console.WriteLine("Details:\n" + e.Message);
+				};
 			}
 			return MissionVersion;
 		}
